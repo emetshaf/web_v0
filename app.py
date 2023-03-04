254ca2a77a98616aa1b954fe2726ba0634fd76fa
Mubarek Seid Juhar <mubareksd@gmail.com>
2023-03-04 14:43:43 +0300

    added edit narrator feature

--- app.py
+++ app.py
@@ -657,6 +657,53 @@ def admin_narrators():
         )
 
 
+@app.route('/admin/edit_narrator/<narrator_id>', methods=['GET', 'POST'], strict_slashes=False)
+def edit_narrator(narrator_id):
+    """Admin Edit Narrator Page
+    """
+    if api_status()['status'] == 'OK':
+        url = "http://localhost/api/v1/narrators/" + narrator_id
+        narrator = requests.get(url).json()
+        if request.method == 'POST':
+            filename = ''
+            if 'file' in request.files:
+                file = request.files['file']
+                if file and allowed_file(file.filename):
+                    extension = os.path.splitext(file.filename)[1]
+                    filename = str(uuid4()) + extension
+                    file.save(os.path.join(
+                        app.config['UPLOAD_FOLDER'] + '/narrators', filename))
+            first_name = request.form['first_name']
+            middle_name = request.form['middle_name']
+            last_name = request.form['last_name']
+            if filename == '':
+                payload = {
+                    'first_name': first_name,
+                    'middle_name': middle_name,
+                    'last_name': last_name
+                }
+            else:
+                payload = {
+                    'first_name': first_name,
+                    'middle_name': middle_name,
+                    'last_name': last_name,
+                    'image': filename
+                }
+            requests.put(url, json=payload)
+            return redirect('/admin/narrators')
+        cache_id = uuid4()
+        return render_template(
+            'admin/edit_narrator.html',
+            narrator=narrator,
+            cache_id=cache_id,
+        )
+    else:
+        return render_template(
+            '500.html',
+            cache_id=cache_id,
+        )
+
+
 @ app.route('/admin/create_narrator', methods=['POST'], strict_slashes=False)
 def create_narrator():
     if request.method == 'POST':
